6817a98bd94b618123a35139efcf3feea46259c4
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-01 13:40:55 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 import GithubCard from "./components/GithubCard.vue";
 </script>
 
-<template></template>
+<template>
+  <GithubCard username="danielkellyio"></GithubCard>
+  <GithubCard username="mehrnaz98"></GithubCard>
+</template>
 
 <style scoped></style>
